41f028af47ad8cde60c6ef8e46e08a9b90b2df62
sena <sena@sena-MS-7817>
2017-05-03 09:42:28 -0500

    v.2

--- src/javaapplication18/Egresado.java
+++ src/javaapplication18/Egresado.java
@@ -10,6 +10,43 @@ package javaapplication18;
  * @author sena
  */
 public class Egresado {
-    private String id;
-    
+     private String idEgresado; 
+      private String nombre; 
+       private String direccion; 
+        private String telefono; 
+         private String correo; 
+    public void setIdEgresado(String idEgresadoP){ 
+        this.idEgresado= idEgresadoP; 
+    } 
+    public String getIdEgresado(){ 
+        return idEgresado; 
+    } public void solicitar(){
+        System.out.println("solicitar_documentos");
+        System.out.println("solicitar_documentos2"); 
+    }
+    public void setNombre(String nombreP){ 
+        this.nombre= nombreP; 
+    } public String getNombre(){
+        return nombre; 
+    } public void ingresar(){}
+    public void setDireccion(String direccionP){ 
+        this.direccion= direccionP; 
+    } 
+    public String getdireccion(){ 
+        return direccion; 
+    } 
+    public void setTelefono(String telefonoP){
+        this.telefono= telefonoP; 
+    } 
+    public String getTelefono(){ 
+        return telefono; 
+    } 
+    public void setCorreo(String correoP){ 
+        this.correo= correoP; 
+    } 
+    public String getCorreo(){ 
+        return correo; 
+    } 
 }
+    
+
